7e56ead82bcac9e1bde05024d66d85aa75f15c0c
chai4hunt7@gmail.com <chai4hunt7@gmail.com>
2023-03-29 14:09:37 +0000

    with fact table

---
+++ models/staging/stripe/stg_payment.sql
@@ -0,0 +1,17 @@
+with payment as (
+
+select
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+
+    -- amount is stored in cents, convert it to dollars
+    amount / 100 as amount,
+    created as created_at
+
+from raw1.stripe1.payment
+
+)
+
+select * from payment
